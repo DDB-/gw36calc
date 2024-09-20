705792b3151158348ba7d8952b6d7e37b56ba181
Ryan <nyrrules@gmail.com>
2024-09-19 21:44:56 -0700

    Selected units into a table view
    All of the available units can now be selected,
    and they get shoved into a little table.

--- src/js/units.js
+++ src/js/units.js
@@ -1,27 +1,32 @@
-const units = ['Infantry', 'Militia', 'Marine', 'Artillery'];
-let attackerUnits = [];
-let defenderUnits = [];
+const infantry = [
+    'Infantry', 'Militia', 'Airborne Infantry', 'Elite Airborne Infantry',
+    'Marine', 'Mountain Infantry', 'Colonial Infantry', 'Foreign Legion', 'Gurkha'
+];
+const artillery = [
+    'Artillery', 'Self-Propelled Artillery', 'Advanced Artillery', 'Anti Aircraft Artillery',
+    'Advanced Self-Propelled Artillery', 'Katyusha'
+];
+const vehicles = [
+    'Cavalry', 'Motorized Infantry', 'Mechanized Infantry', 'Advanced Mechanized Infantry',
+    'Panzer Grenadier', 'Tank Destroyer', 'Light Tank', 'Medium Tank', 'T-34',
+    'Heavy Tank', 'Tiger I'
+];
+const planes = [
+    'Fighter', 'Jet Fighter', 'Tactical Bomber', 'Medium Bomber', 'Strategic Bomber',
+    'Heavy Strategic Bomber', 'Seaplane', 'Air Transport', 'Heavy Air Transport'
+];
+const boats = [
+    'Torpedo Boat Destroyer', 'Destroyer', 'Coastal Defense Ship', 'Light Cruiser',
+    'Heavy Cruiser', 'Battlecruiser', 'Battleship', 'Heavy Battleship', 'Light Carrier',
+    'Fleet Carrier', 'Heavy Fleet Carrier', 'Coastal Submarine', 'Submarine', 'Advanced Submarine',
+    'Naval Transport', 'Attack Transport'
+];
+const units = [...infantry, ...artillery, ...vehicles, ...planes, ...boats];
 
 function getUnits() {
     return units;
 }
 
-function getUnitsForSide(side) {
-    if (side === "attack") {
-        return attackerUnits;
-    } else {
-        return defenderUnits;
-    }
-}
-
-function addUnit(side, unit) {
-    if (units.indexOf(unit) == -1) {
-        console.log(unit);
-        return;
-    }
-    if (side === "attack") {
-        attackerUnits.push(unit)
-    } else {
-        defenderUnits.push(unit)
-    }
+function isUnit(unit) {
+    return (units.indexOf(unit) != -1);
 }
