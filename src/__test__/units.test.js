472ddde9fec01484db35b5bcd5c68310ca90d674
Ryan <nyrrules@gmail.com>
2024-09-22 19:32:12 -0700

    Add all units with attack, defend, and cost
    Ported over all of the files from the old python
    project into this one so that the pips can be immediately
    accessed in this one. Added tests to ensure all units
    are constructing properly, and also use jest-chain because
    I like how the chaining works.

--- src/__test__/units.test.js
+++ src/__test__/units.test.js
@@ -1,14 +1,5 @@
 const script = (url) => {
-    const { protocol } = new URL(url, 'file://');
-    switch (protocol) {
-        case 'file:':
-            return require('fs').readFileSync(`${process.cwd()}/${url}`, 'UTF8');
-        case 'http:':
-        case 'https:':
-            return String(require('child_process').execSync(`wget -O - -o /dev/null '${url}'`))
-        default:
-            throw new Error('unsupported protocol');
-    }
+    return require('fs').readFileSync(`${process.cwd()}/${url}`, 'UTF8');
 };
 
 eval(script('src/js/units.js'));
@@ -37,4 +28,41 @@ test('getUnitType correctly identifies unit type', () => {
     expect(getUnitType('Medium Bomber')).toBe('Plane');
     expect(getUnitType('Self-Propelled Artillery')).toBe('Artillery');
     expect(getUnitType('Foo Fighter')).toBe('Invalid');
+});
+
+test('Unit class constructs as expected', () => {
+    const unit = makeUnit('Marine', 3);
+    expect(unit.name).toBe('Marine');
+    expect(unit.quantity).toBe(3);
+    expect(unit.unitClass).toBe('Infantry');
+    expect(unit.details.get('Attack')).toBe(2);
+    expect(unit.details.get('Defend')).toBe(4);
+});
+
+test('all units construct with values', () => {
+    const allUnits = getUnits();
+    allUnits.forEach((unitName, index) => {
+        const unit = makeUnit(unitName, index);
+        expect(unit.name).toBe(unitName);
+        expect(unit.quantity).toBe(index);
+        expect(unit.details.get('Attack'))
+            .toBeDefined()
+            .toBeGreaterThanOrEqual(0)
+            .toBeLessThan(12);
+        expect(unit.details.get('Defend'))
+            .toBeDefined()
+            .toBeGreaterThanOrEqual(0)
+            .toBeLessThan(12);
+        
+        const cost = unit.details.get('Cost');
+        expect(cost).toBeDefined()
+        if (Array.isArray(cost)) {
+            cost.forEach((stageCost) => {
+                expect(stageCost).toBeGreaterThanOrEqual(0);
+            });
+        } else {
+            expect(cost).toBeGreaterThanOrEqual(0);
+        }
+        
+    });
 });
--- src/js/units.js
+++ src/js/units.js
@@ -55,3 +55,277 @@ function getUnitType(unit) {
 
     return "Invalid";
 }
+
+function getUnitDetails(unit) {
+    let details = new Map();
+    switch(unit) {
+        case 'Militia':
+            details.set('Attack', 1);
+            details.set('Defend', 2);
+            details.set('Cost', 2);
+            break;
+        case 'Infantry':
+            details.set('Attack', 2);
+            details.set('Defend', 4);
+            details.set('Cost', 3);
+            break;
+        case 'Airborne Infantry':
+            details.set('Attack', 2);
+            details.set('Defend', 2);
+            details.set('Cost', 3);
+            break;
+        case 'Elite Airborne Infantry':
+            details.set('Attack', 3);
+            details.set('Defend', 3);
+            details.set('Cost', 3);
+            break;
+        case 'Marine':
+            details.set('Attack', 2);
+            details.set('Defend', 4);
+            details.set('Cost', 4);
+            break;
+        case 'Mountain Infantry':
+            details.set('Attack', 2);
+            details.set('Defend', 4);
+            details.set('Cost', 4);
+            break;
+        case 'Colonial Infantry':
+            details.set('Attack', 2);
+            details.set('Defend', 4);
+            details.set('Cost', 4);
+            break;
+        case 'Foreign Legion':
+            details.set('Attack', 2);
+            details.set('Defend', 4);
+            details.set('Cost', 4);
+            break;
+        case 'Gurkha':
+            details.set('Attack', 2);
+            details.set('Defend', 4);
+            details.set('Cost', 4);
+            break;
+        case 'Artillery':
+            details.set('Attack', 3);
+            details.set('Defend', 3);
+            details.set('Cost', 4);
+            break;
+        case 'Self-Propelled Artillery':
+            details.set('Attack', 3);
+            details.set('Defend', 3);
+            details.set('Cost', 5);
+            break;
+        case 'Advanced Artillery':
+            details.set('Attack', 4);
+            details.set('Defend', 4);
+            details.set('Cost', 4);
+            break;
+        case 'Advanced Self-Propelled Artillery':
+            details.set('Attack', 4);
+            details.set('Defend', 4);
+            details.set('Cost', 5);
+            break;
+        case 'Katyusha':
+            details.set('Attack', 5);
+            details.set('Defend', 4);
+            details.set('Cost', 5);
+            break;
+        case 'Anti Aircraft Artillery':
+            details.set('Attack', 3);
+            details.set('Defend', 3);
+            details.set('Cost', 4);
+        case 'Torpedo Boat Destroyer':
+            details.set('Attack', 2);
+            details.set('Defend', 2);
+            details.set('Cost', 0);
+            break;
+        case 'Destroyer':
+            details.set('Attack', 4);
+            details.set('Defend', 4);
+            details.set('Cost', 7);
+            break;
+        case 'Coastal Defense Ship':
+            details.set('Attack', 4);
+            details.set('Defend', 6);
+            details.set('Cost', 0);
+            break;
+        case 'Light Cruiser':
+            details.set('Attack', 5);
+            details.set('Defend', 5);
+            details.set('Cost', 9);
+            break;
+        case 'Heavy Cruiser':
+            details.set('Attack', 6);
+            details.set('Defend', 6);
+            details.set('Cost', [5, 5]);
+            break;
+        case 'Battlecruiser':
+            details.set('Attack', 7);
+            details.set('Defend', 7);
+            details.set('Cost', [7, 7]);
+            break;
+        case 'Battleship':
+            details.set('Attack', 8);
+            details.set('Defend', 8);
+            details.set('Cost', [6, 6, 6]);
+            break;
+        case 'Heavy Battleship':
+            details.set('Attack', 10);
+            details.set('Defend', 10);
+            details.set('Cost', [7, 7, 7]);
+            break;
+        case 'Light Carrier':
+            details.set('Attack', 0);
+            details.set('Defend', 1);
+            details.set('Cost', [4, 4]);
+            break;
+        case 'Fleet Carrier':
+            details.set('Attack', 0);
+            details.set('Defend', 2);
+            details.set('Cost', [5, 5, 5]);
+            break;
+        case 'Heavy Fleet Carrier':
+            details.set('Attack', 0);
+            details.set('Defend', 2);
+            details.set('Cost', [6, 6, 6]);
+            break;
+        case 'Coastal Submarine':
+            details.set('Attack', 2);
+            details.set('Defend', 2);
+            details.set('Cost', 0);
+            break;
+        case 'Submarine':
+            details.set('Attack', 3);
+            details.set('Defend', 3);
+            details.set('Cost', 6);
+            break;
+        case 'Advanced Submarine':
+            details.set('Attack', 4);
+            details.set('Defend', 4);
+            details.set('Cost', 7);
+            break;
+        case 'Naval Transport':
+            details.set('Attack', 0);
+            details.set('Defend', 0);
+            details.set('Cost', 7);
+            break;
+        case 'Attack Transport':
+            details.set('Attack', 0);
+            details.set('Defend', 1);
+            details.set('Cost', 7);
+            break;
+        case 'Fighter':
+            details.set('Attack', 6);
+            details.set('Defend', 6);
+            details.set('Cost', 10);
+            break;
+        case 'Jet Fighter':
+            details.set('Attack', 8);
+            details.set('Defend', 8);
+            details.set('Cost', 12);
+            break;
+        case 'Tactical Bomber':
+            details.set('Attack', 7);
+            details.set('Defend', 5);
+            details.set('Cost', 11);
+            break;
+        case 'Medium Bomber':
+            details.set('Attack', 7);
+            details.set('Defend', 4);
+            details.set('Cost', 11);
+            break;
+        case 'Strategic Bomber':
+            details.set('Attack', 2);
+            details.set('Defend', 2);
+            details.set('Cost', 12);
+            break;
+        case 'Heavy Strategic Bomber':
+            details.set('Attack', 2);
+            details.set('Defend', 3);
+            details.set('Cost', 13);
+            break;
+        case 'Seaplane':
+            details.set('Attack', 3);
+            details.set('Defend', 3);
+            details.set('Cost', 7);
+            break;
+        case 'Air Transport':
+            details.set('Attack', 0);
+            details.set('Defend', 0);
+            details.set('Cost', 8);
+            break;
+        case 'Heavy Air Transport':
+            details.set('Attack', 0);
+            details.set('Defend', 0);
+            details.set('Cost', 10);
+            break;
+        case 'Cavalry':
+            details.set('Attack', 3);
+            details.set('Defend', 2);
+            details.set('Cost', 3);
+            break;
+        case 'Motorized Infantry':
+            details.set('Attack', 2);
+            details.set('Defend', 4);
+            details.set('Cost', 4);
+            break;
+        case 'Mechanized Infantry':
+            details.set('Attack', 3);
+            details.set('Defend', 4);
+            details.set('Cost', 4);
+            break;
+        case 'Advanced Mechanized Infantry':
+            details.set('Attack', 4);
+            details.set('Defend', 5);
+            details.set('Cost', 4);
+            break;
+        case 'Panzer Grenadier':
+            details.set('Attack', 4);
+            details.set('Defend', 5);
+            details.set('Cost', 4);
+            break;
+        case 'Tank Destroyer':
+            details.set('Attack', 3);
+            details.set('Defend', 4);
+            details.set('Cost', 5);
+            break;
+        case 'Light Tank':
+            details.set('Attack', 3);
+            details.set('Defend', 1);
+            details.set('Cost', 4);
+            break;
+        case 'Medium Tank':
+            details.set('Attack', 6);
+            details.set('Defend', 5);
+            details.set('Cost', 6);
+            break;
+        case 'T-34':
+            details.set('Attack', 6);
+            details.set('Defend', 5);
+            details.set('Cost', 5);
+            break;
+        case 'Heavy Tank':
+            details.set('Attack', 8);
+            details.set('Defend', 7);
+            details.set('Cost', 7);
+            break;
+        case 'Tiger I':
+            details.set('Attack', 8);
+            details.set('Defend', 7);
+            details.set('Cost', 7);
+            break;
+    }
+    return details;
+}
+
+class Unit {
+    constructor(name, quantity) {
+        this.name = name;
+        this.quantity = quantity;
+        this.unitClass = getUnitType(name);
+        this.details = getUnitDetails(name);
+    }
+}
+
+function makeUnit(name, quantity) {
+    return new Unit(name, quantity);
+}
