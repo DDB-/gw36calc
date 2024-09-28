73962370f4bffab4a832c6e91aa41a9c0e921d46
Ryan <nyrrules@gmail.com>
2024-09-27 23:10:18 -0700

    Add terrain calculations
    They are not integrated, but the calculations for
    the terrain modifiers are there. The idea is that
    you can have multiple modifiers from terrain, and those
    will go into the full list for each unit, whereby
    each unit will have the max and min selected for
    them per rule 0.5b.

--- src/__test__/modifiers.test.js
+++ src/__test__/modifiers.test.js
@@ -2,6 +2,7 @@ const script = (path) => {
     return require('fs').readFileSync(`${process.cwd()}/${path}`, 'UTF8');
 };
 
+eval(script('src/js/units.js'));
 eval(script('src/js/modifiers.js'));
 
 test('getters return expected list', () => {
@@ -10,4 +11,44 @@ test('getters return expected list', () => {
 
     const terrains = getTerrains();
     expect(terrains.length).toBe(6);
+});
+
+test('get expected terrain modifiers', () => {
+    expect(getTerrainModifier('Normal', makeUnit('Infantry', 1), 'Attack', 1)).toBe(0);
+
+    expect(getTerrainModifier('Mountains', makeUnit('Infantry', 1), 'Attack', 1)).toBe(-1);
+    expect(getTerrainModifier('Mountains', makeUnit('Infantry', 1), 'Defend', 1)).toBe(0);
+    expect(getTerrainModifier('Mountains', makeUnit('Mountain Infantry', 1), 'Attack', 1)).toBe(0);
+    expect(getTerrainModifier('Mountains', makeUnit('Foreign Legion', 1), 'Defend', 1)).toBe(1);
+    expect(getTerrainModifier('Mountains', makeUnit('Medium Tank', 1), 'Attack', 1)).toBe(-1);
+    expect(getTerrainModifier('Mountains', makeUnit('Fighter', 1), 'Defend', 1)).toBe(0);
+
+    expect(getTerrainModifier('Desert', makeUnit('Infantry', 1), 'Attack', 1)).toBe(-1);
+    expect(getTerrainModifier('Desert', makeUnit('Artillery', 1), 'Attack', 1)).toBe(-1);
+    expect(getTerrainModifier('Desert', makeUnit('Artillery', 1), 'Defend', 1)).toBe(-1);
+    expect(getTerrainModifier('Desert', makeUnit('Foreign Legion', 1), 'Defend', 1)).toBe(0);
+    expect(getTerrainModifier('Desert', makeUnit('Foreign Legion', 1), 'Attack', 1)).toBe(1);
+
+    expect(getTerrainModifier('Jungle', makeUnit('Infantry', 1), 'Attack', 1)).toBe(0);
+    expect(getTerrainModifier('Jungle', makeUnit('Medium Tank', 1), 'Attack', 1)).toBe(-2);
+    expect(getTerrainModifier('Jungle', makeUnit('Light Tank', 1), 'Defend', 1)).toBe(-2);
+    expect(getTerrainModifier('Jungle', makeUnit('Gurkha', 1), 'Attack', 1)).toBe(1);
+    expect(getTerrainModifier('Jungle', makeUnit('Gurkha', 1), 'Defend', 1)).toBe(0);
+
+    expect(getTerrainModifier('River', makeUnit('Infantry', 1), 'Attack', 1)).toBe(-1);
+    expect(getTerrainModifier('River', makeUnit('Infantry', 1), 'Attack', 2)).toBe(0);
+    expect(getTerrainModifier('River', makeUnit('Infantry', 1), 'Defend', 1)).toBe(0);
+    expect(getTerrainModifier('River', makeUnit('Marine', 1), 'Attack', 1)).toBe(0);
+    expect(getTerrainModifier('River', makeUnit('Medium Tank', 1), 'Attack', 1)).toBe(-1);
+    expect(getTerrainModifier('River', makeUnit('Jet Fighter', 1), 'Attack', 1)).toBe(0);
+
+    expect(getTerrainModifier('City', makeUnit('Infantry', 1), 'Attack', 1)).toBe(0);
+    expect(getTerrainModifier('City', makeUnit('Infantry', 1), 'Defend', 1)).toBe(1);
+    expect(getTerrainModifier('City', makeUnit('Medium Tank', 1), 'Attack', 1)).toBe(0);
+    expect(getTerrainModifier('City', makeUnit('Jet Fighter', 1), 'Attack', 1)).toBe(0);
+
+    getUnits().forEach((unit) => {
+        expect(getTerrainModifier('Surrounded City', makeUnit(unit, 1), 'Attack', 1)).toBe(0);
+        expect(getTerrainModifier('Surrounded City', makeUnit(unit, 1), 'Defend', 1)).toBe(-1);
+    });
 });
--- src/js/modifiers.js
+++ src/js/modifiers.js
@@ -1,7 +1,7 @@
 const facilities = ['Fortification', 'Coastal Artillery'];
 const battleTypes = ['Land', 'Amphibious Assault', 'Naval'];
 const terrains = ['Normal', 'Mountains', 'Desert', 'Jungle', 'Marshes', 'Tundra/Ice'];
-const terrainMods = ['City', 'River'];
+const terrainMods = ['Surrounded City', 'City', 'River'];
 
 function getFacilities() {
     return facilities;
@@ -17,4 +17,52 @@ function getTerrains() {
 
 function getTerrainMods() {
     return terrainMods;
+}
+
+function getTerrainModifier(terrain, unit, side, round) {
+    const landUnits = ['Infantry', 'Artillery', 'AntiAir', 'Vehicle'];
+    if (terrain === 'Mountains') {
+        if (landUnits.indexOf(unit.unitClass) != -1) {
+            if (['Mountain Infantry', 'Foreign Legion', 'Gurkha'].indexOf(unit.name) != -1) {
+                return (side === 'Attack') ? 0 : 1;
+            } else {
+                return (side === 'Attack') ? -1 : 0;
+            }
+        }
+    } else if (terrain === 'Desert') {
+        if (landUnits.indexOf(unit.unitClass) != -1) {
+            if (['Foreign Legion'].indexOf(unit.name) != -1) {
+                return (side === 'Attack') ? 1 : 0;
+            } else {
+                return -1;
+            }
+        }
+    } else if (terrain === 'Jungle') {
+        if (['Vehicle'].indexOf(unit.unitClass) != -1) {
+            return -2;
+        } else if (['Gurkha'].indexOf(unit.name) != -1 && side === 'Attack') {
+            return 1;
+        }
+    } else if (terrain === 'Marshes') {
+        if (['Vehicle'].indexOf(unit.unitClass) != -1) {
+            return -1;
+        }
+    } else if (terrain === 'River') {
+        // TODO cancel penalty for Airborne Infantry if they are participating in airborne assault
+        if (landUnits.indexOf(unit.unitClass) != -1
+            && ['Marine'].indexOf(unit.name) == -1
+            && side === 'Attack' && round === 1)
+        {
+            return -1;
+        }
+    } else if (terrain === 'City') {
+        // TODO Will need to come back and add target select soon
+        if ('Infantry' === unit.unitClass && side === 'Defend') {
+            return 1;
+        }
+    } else if (terrain === 'Surrounded City') {
+        return (side === 'Defend') ? -1 : 0;
+    }
+
+    return 0;
 }
